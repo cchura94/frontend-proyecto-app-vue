e37b2fd4bba699ee8291b2092e3bcd2b3e4dd6c3
cchura94 <cchura.cpc@gmail.com>
2021-09-24 22:10:53 -0400

    CRUD Sucursal

--- src/views/admin/sucursal/SucursalEditar.vue
+++ src/views/admin/sucursal/SucursalEditar.vue
@@ -1,9 +1,83 @@
 <template>
-  <h1>Editar Sucursal</h1>
+<div>
+    <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+            <v-flex xs12 sm8 md8>
+                <v-card class="elevation-12">
+                    <v-toolbar dark color="primary">
+                    <v-toolbar-title>Editar Sucursal</v-toolbar-title>
+                    </v-toolbar>
+                    <v-card-text>
+
+     <v-form
+        ref="form"
+        v-model="valid"
+        lazy-validation
+      >
+        <v-text-field
+          v-model="sucursal.nombre"
+          :counter="50"
+          :rules="nombreRules"
+          label="Nombre Sucursal"
+          required
+        ></v-text-field>
+
+        <v-text-field
+          v-model="sucursal.telefono"
+          :counter="12"
+          label="ingrese Telefono"
+          required
+        ></v-text-field>
+
+        <v-text-field
+          v-model="sucursal.direccion"
+          label="ingrese Dirección"
+          required
+        ></v-text-field>
+
+    <v-btn color="primary" @click="modificarSucursal()" :disabled="!valid">Guardar Sucursal</v-btn>
+     </v-form>
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+    
+  </div>
 </template>
 
 <script>
+import axios from "axios"
+
 export default {
+  data(){
+    return {
+       sucursal: {
+          nombre: "",
+          telefono: "",
+          direccion: "",
+          user_id: 1
+        },
+        valid: true,
+        nombreRules: [
+          v => !!v || 'El nombre de la Sucursal es Oblogatoria',
+          v => (v && v.length <= 50) || 'Debe contener menos de 50 caracteres',
+        ],
+    }
+  },
+  async created(){
+    let id = this.$route.params.id
+    const { data } = await axios.get("http://127.0.0.1:8000/api/sucursal/"+id);
+    this.sucursal = data
+  },
+  methods:{
+    async modificarSucursal(){
+      let id = this.$route.params.id
+
+      const { data } = await axios.put("http://127.0.0.1:8000/api/sucursal/"+id, this.sucursal);
+      this.$router.push("/admin/sucursal")
+    }
+  }
 
 }
 </script>
--- src/views/admin/sucursal/SucursalList.vue
+++ src/views/admin/sucursal/SucursalList.vue
@@ -1,13 +1,69 @@
 <template>
-  <h1>Lista de Sucursales</h1>
+  <div>
+    <v-btn color="primary" to="/admin/sucursal/nuevo">
+      Nueva Sucursal
+    </v-btn>
+
+    <v-card class="elevation-12">
+      <v-card-text>
+
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th v-for="(col, i) in columnas" :key="i">{{ col }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(sucursal, i) in lista_sucursales" :key="i">
+            <td>{{ sucursal.nombre }}</td>
+            <td>{{ sucursal.telefono }}</td>
+            <td>{{ sucursal.direccion }}</td>
+            <td>
+              <v-btn icon color="warning" :to="`/admin/sucursal/${sucursal.id}/editar`">
+                <v-icon>mdi-pencil</v-icon>
+              </v-btn>
+
+              <v-btn icon color="danger" @click="eliminarSucursal(sucursal, i)">
+                <v-icon>mdi-pencil</v-icon>
+              </v-btn>
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
+import axios from "axios"
+
 export default {
+  data(){
+    return {
+      columnas: ["Nombre", "Telefono", "Dirección", "Acciones"],
+      lista_sucursales: [
+        /* {nombre: "Casa matriz", telefono: "2342342", direccion: "Av 123 Abc"},
+        {nombre: "Sucursal 2", telefono: "2342342", direccion: "Av 123 Abc"}
+        */
+      ]
+    }
+  },
+  async created(){
+    const respuesta = await axios.get('http://127.0.0.1:8000/api/sucursal');
+    console.log(respuesta)
+    this.lista_sucursales = respuesta.data
+  },
+  methods: {
+    async eliminarSucursal(sucursal, pos){
+      if(confirm("Está seguro de eliminar la Sucursal?")){
+        await axios.delete('http://127.0.0.1:8000/api/sucursal/'+sucursal.id);
+      this.lista_sucursales.splice(pos, 1);
+      }
+    }
+  }
 
 }
 </script>
-
-<style>
-
-</style>
